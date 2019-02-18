0a97010998c2a99387648ed0f7cad8ec445fd975
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-02-18 16:32:32 +0100

    Initial Commit

---
+++ SDL Toturial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL Toturial
+//
+//  Created by Henrik Anthony Odden Sandberg on 2/18/19.
+//  Copyright © 2019 Henrik Anthony Odden Sandberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
